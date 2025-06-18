67f7e2f31cebe027122658d9b8b79b38a825a712
csdivyansh <idivyanshv@gmail.com>
2025-06-18 12:11:53 +0530

    add nodejs-assignment

---
+++ NodeJS-Assignment/greetings.mjs
@@ -0,0 +1,5 @@
+function greet(name) {
+    let msg = `Hello, ${name}! Welcome to the Node.js module system`
+    return msg
+}
+export default greet;
---
+++ NodeJS-Assignment/main.mjs
@@ -0,0 +1,8 @@
+// const greet = require('./greetings.mjs')
+import greet from "./greetings.mjs"
+import {add, subtract, multiply, divide} from "./mathOperations.mjs"
+console.log(greet("Divyansh Varshney"));
+console.log(add(1, 2))
+console.log(subtract(1, 2))
+console.log(multiply(1, 2))
+console.log(divide(1, 2))
